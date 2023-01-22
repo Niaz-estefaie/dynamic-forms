36aac88910e2a8936b0ee0416581ba7cc79dd662
Niaz <estefaie.niaz@gmail.com>
2023-01-22 11:11:34 +0330

    build: :package: new packages and dependencies added to the project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Datepicker from "@vuepic/vue-datepicker";
+import { VueDraggableNext } from 'vue-draggable-next'
+import "@vuepic/vue-datepicker/dist/main.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component("Datepicker", Datepicker);
+app.component("Draggable", VueDraggableNext);
+
+app.mount("#app");
